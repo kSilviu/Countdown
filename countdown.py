026dcc891e80865b39c1df7eed025eb91fe6b4f8
Silviu Hostinariu <silviuhostinariumihai@gmail.com>
2026-01-02 00:56:48 +0000

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -4,13 +4,13 @@ def countdown():
     seconds = 0
     seconds = int(input("Enter time needed in seconds"))
 
+    if seconds == 0:
+        print("Countdown is over")
+
     while seconds != 0:
         time.sleep(1)
         seconds -= 1
         time.sleep(1)
         print(seconds+1, "seconds left!")
-
-    if seconds == 0:
-        print("Countdown is over")
 
 countdown()
